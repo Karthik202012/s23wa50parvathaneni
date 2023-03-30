6b90b6c8fb5963c2dba46bd5c1ee353378a3a84a
Karthik Parvathaneni <123134732+Karthik202012@users.noreply.github.com>
2023-03-29 20:10:07 -0500

    updated

--- routes/computation.js
+++ routes/computation.js
@@ -1,34 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
-
 /* Interesting fact about me */
 router.get('/', function(req, res, next) {
     var x;
     var rand = Math.random();
     console.log(req.query.x);
     num = req.query.x;
-//   res.render('mydata', { title: 'Praveen Babu Narni' });
-x =3
+//   res.render('mydata', { title: 'Karthik Parvathaneni' });
+x =2
 //  req.params()
 if(x == undefined){
     x=rand;
 }
 
-let cos = Math.acosh(x);
-let log = Math.log1p(x);
-let tab = Math.tan(x);
-let sq = Math.sqrt(x);
+let cos = Math.fround(x);
+let log = Math.random(x);
+let tab = Math.round(x);
 
 res.render('computation',{
     title:'computation',
-    values:'Math.acosh() applied to '+x+' is '+cos,
-    value1:'Math.log1p() applied to '+x+' is '+log,
-    value2:'Math.tan() applied to '+x+' is '+tab, 
-    values3:'Math.sqrt() applied to '+x+' is '+sq
+    values:'Math.fround() applied to '+x+' is '+cos,
+    value1:'Math.random() applied to '+x+' is '+log,
+    value2:'Math.round() applied to '+x+' is '+tab, 
+  
 });
 });
-
-
-
 module.exports = router;
